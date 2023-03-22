e353f0c18a790c79b2257ce4e7ecd273429b56ea
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-22 07:59:45 +0530

    add database connection

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import cors from 'cors';
+import connectDB from './config/connectdb.js'
 
 const app = express()
 const port = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+
+app.use(cors())
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
